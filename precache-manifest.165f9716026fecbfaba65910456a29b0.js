30c993ccf1cca510540998d1e7abf9eba70bea27
PinewoodPip <53646914+PinewoodPip@users.noreply.github.com>
2020-07-23 12:50:01 +0200

    Updates

---
+++ precache-manifest.165f9716026fecbfaba65910456a29b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bce8f76e8f34687e13a382eff2d10d",
+    "url": "/ee2calc/index.html"
+  },
+  {
+    "revision": "34f0529aba7a827ef499",
+    "url": "/ee2calc/static/css/2.a8f673c4.chunk.css"
+  },
+  {
+    "revision": "10d690cf3a3ed68d5f72",
+    "url": "/ee2calc/static/css/main.03e67db6.chunk.css"
+  },
+  {
+    "revision": "34f0529aba7a827ef499",
+    "url": "/ee2calc/static/js/2.5409a51f.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/ee2calc/static/js/2.5409a51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d690cf3a3ed68d5f72",
+    "url": "/ee2calc/static/js/main.41f08b7d.chunk.js"
+  },
+  {
+    "revision": "57bd3969c51cbf2a98a8",
+    "url": "/ee2calc/static/js/runtime-main.70ba40fe.js"
+  }
+]);
